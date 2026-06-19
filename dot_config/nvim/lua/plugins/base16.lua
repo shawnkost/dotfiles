b0d0e07343a5078e112fb0bd64c28cc57f3946f0
Shawn Kost <shawnmkost@gmail.com>
2026-06-19 16:38:41 -0700

    remove nvim config

--- dot_config/nvim/lua/plugins/base16.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "RRethy/base16-nvim",
-  enabled = false,
-  priority = 2000,
-  config = function()
-    require("matugen").setup()
-  end,
-}
--- dot_config/nvim/lua/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  opts = function(_, opts)
-    opts.options.theme = "auto"
-    opts.options.globalstatus = true
-    opts.options.section_separators = { "", "" }
-    opts.options.component_separators = { "", "" }
-  end,
-}
